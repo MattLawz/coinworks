fc74a80b029383a4eb09f0fbdeb9d7974f3a8f29
geneva <34730060+geenva@users.noreply.github.com>
2021-04-27 21:09:50 +0800

    feat: dark mode by default

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { useColorMode } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
+    const { toggleColorMode } = useColorMode();
     return (
+        {toggleColorMode}
         <ChakraProvider>
             <Component {...pageProps} />
         </ChakraProvider>
