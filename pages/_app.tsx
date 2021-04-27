08a041ec0d997e499e24e76fff49f9dc75eec889
Matt <matthewgorgees@gmail.com>
2021-04-27 08:36:02 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
 
-const theme = extendTheme({
-  config: {
-    useSystemColorMode: true,
-    initialColorMode: "dark"
-  }
-});
-
-
 function MyApp({ Component, pageProps }) {
     return (
         <ChakraProvider>
