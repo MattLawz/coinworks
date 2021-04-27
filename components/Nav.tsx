64f262c7184eaa6a11d220cfdf37167022082c73
Matthew Gorgees <matt@mattlawz.dev>
2021-04-26 22:36:47 -0400

    🚀 First Deployment

---
+++ components/Nav.tsx
@@ -0,0 +1,96 @@
+  
+import React from 'react'
+import {
+    useColorMode,
+    Button,
+    Flex,
+    Box
+} from '@chakra-ui/react'
+import NextLink from 'next/link'
+import styled from '@emotion/styled'
+
+import DarkModeSwitch from './DarkModeSwitch'
+
+const Nav = ({ children }) => {
+    const { colorMode } = useColorMode()
+
+    const bgColor = {
+        light: 'white',
+        dark: '#1A202C'
+    }
+
+    const color = {
+        light: 'black',
+        dark: 'white'
+    }
+
+    const navHoverBg = {
+        light: 'gray.600',
+        dark: 'gray.300',
+    }
+
+    const StickyNav = styled(Flex)`
+        position: sticky;
+        z-index: 10;
+        top: 0;
+        backdrop-filter: saturate(180%) blur(20px);
+        transition: height .5s, line-height .5s;
+        `
+
+    return (
+        <>
+            <StickyNav
+                flexDirection="row"
+                justifyContent="space-between"
+                alignItems="center"
+                maxWidth="800px"
+                minWidth="356px"
+                width="100%"
+                bg={bgColor[colorMode]}
+                as="nav"
+                px={[2, 6, 6]}
+                py={2}
+                mt={8}
+                mb={[0, 0, 8]}
+                mx="auto"
+            >
+                <Box>
+                    <NextLink href="https://donatebtc.vercel.app" passHref>
+                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
+                            Home
+                        </Button>
+                    </NextLink>
+                    <NextLink href="https://app.archbee.io/doc/9vcLAEz22e8v64S5Y2Ck3/78sRUU71EiCZ4NHolJYqN" passHref>
+                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
+                            Documentation
+                        </Button>
+                    </NextLink>
+                    <NextLink href="https://app.archbee.io/doc/ByB0SITocaEpKZ1Y6QzTf/JLZfnLzWITtp3tqRXEOaK" passHref>
+                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
+                            Why Us
+                        </Button>
+                    </NextLink>
+                    <NextLink href="https://github.com/MattLawz/bitcoin-donate" passHref>
+                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
+                            GitHub
+                        </Button>
+                    </NextLink>
+                </Box>
+                <DarkModeSwitch />
+            </StickyNav >
+            <Flex
+                as="main"
+                justifyContent="center"
+                flexDirection="column"
+                bg={bgColor[colorMode]}
+                color={color[colorMode]}
+                px={[0, 4, 4]}
+                mt={[4, 8, 8]}
+            >
+                {children}
+            </Flex>
+        </>
+    )
+}
+
+export default Nav
---
+++ pages/index.tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import { Heading, Stack, Center, Text, Button } from '@chakra-ui/react'
+import Nav from '../components/Nav'
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+import Link from 'next/link'
+
+export default function Home() {
+  return (<>
+<Head>
+<title>Donate BTC</title>
+      <link rel="icon" href="/btc.svg" />
+      <meta name="og:type" content="website" />
+      <meta
+        name="description"
+        content="Custom Donate BTC Page"
+      />
+      <meta name="keywords" content="donate bitcoin, coinbase, donation" />
+      <meta
+        name="og:description"
+        content="Custom Donate BTC Page"
+      />
+      <meta name="og:image" content="/btc.svg" />
+      <meta name="theme-color" content="#f2a900" />
+</Head>
+<Nav />
+<Center>
+<Stack spacing={8}>
+<Heading>Custom Bitcoin Donation Pages</Heading>
+<Text>There is no registration or logins, it is meant to be easy, fast, and loaded with features.</Text>
+<Link href="https://app.archbee.io/doc/9vcLAEz22e8v64S5Y2Ck3/78sRUU71EiCZ4NHolJYqN">
+        <a target="_blank" rel="noreferer">
+<Button rightIcon={<ArrowForwardIcon />} colorScheme="teal" variant="outline">
+    Get Started
+  </Button>
+  </a>
+  </Link>
+  <Link href="https://app.archbee.io/doc/ByB0SITocaEpKZ1Y6QzTf/JLZfnLzWITtp3tqRXEOaK">
+        <a target="_blank" rel="noreferer">
+<Button rightIcon={<ArrowForwardIcon />} colorScheme="teal" variant="outline">
+    Learn More
+  </Button>
+  </a>
+  </Link>
+
+</Stack>
+</Center>
+
+</>
+)
+}
