8b90f781304c120d9451c06cd58283d2ab4197b8
geneva <34730060+geenva@users.noreply.github.com>
2021-04-27 21:13:33 +0800

    revert

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { useColorMode } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-    const { toggleColorMode } = useColorMode();
     return (
-        {toggleColorMode}
         <ChakraProvider>
             <Component {...pageProps} />
         </ChakraProvider>
