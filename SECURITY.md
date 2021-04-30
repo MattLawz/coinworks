5b5484a4ac9bf4c35fb69070120b2de475fecba2
Matthew Gorgees <matt@mattlawz.dev>
2021-04-30 13:02:32 -0400

    testing out a quick create page

--- SECURITY.md
+++
@@ -1,7 +0,0 @@
-# Security Policy
-
-If you find a security vulnrability, please contact me below:
-
-Discord: `Matt_Lawz#2170`
-
-Email: `matt@mattlawz.dev`
--- components/Container.tsx
+++ components/Container.tsx
@@ -34,6 +34,7 @@ const Container = ({ children }: ContainerProps) => {
     `;
 
     return (
+        
         <>
             <StickyNav
                 flexDirection="row"
