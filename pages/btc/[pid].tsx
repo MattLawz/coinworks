e28b52cd05910b240408964ccb58c3f0f2979880
geenva <geenva@users.noreply.github.com>
2021-04-30 21:40:16 +0800

    feat: invalid address warning

--- pages/btc/[pid].tsx
+++ pages/btc/[pid].tsx
@@ -1,13 +1,25 @@
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Link from "next/link";
-import { Code, Stack, Center, Heading, Tooltip, Image } from "@chakra-ui/react";
+import {
+    Code,
+    Stack,
+    Center,
+    Heading,
+    Tooltip,
+    Image,
+    Text,
+} from "@chakra-ui/react";
 import Container from "../../components/Container";
 import Copy from "../../components/Copy";
+import cryptaddress from "cryptaddress-validator";
+import { WarningIcon, WarningTwoIcon } from "@chakra-ui/icons";
+import { AiFillWarning, AiOutlineWarning } from "react-icons/ai";
 
 export default function btc() {
     const router = useRouter();
     const { pid } = router.query;
+    const valid = cryptaddress("btc").test(pid);
 
     return (
         <>
@@ -28,6 +40,11 @@ export default function btc() {
                 <Stack spacing={8}>
                     <Heading>Donate Bitcoin To</Heading>
                     <Code>{pid}</Code>
+                    {valid ? null : (
+                        <Text align="center" color="pink">
+                            <WarningTwoIcon /> This address might not be valid.
+                        </Text>
+                    )}
                     {
                         // @ts-ignore
                         <Copy btc={pid} />
