f3097bb1c94423315015fc403b4640d6536c3a8e
Matthew Gorgees <matt@mattlawz.dev>
2021-04-30 13:21:12 -0400

    reverting back to older version because something broke

--- pages/api/verify.tsx
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import WAValidator from "@swyftx/api-crypto-address-validator";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const valid = WAValidator.validate(req.query.key, "BTC");
-    res.status(200).json({ valid: valid });
-}
