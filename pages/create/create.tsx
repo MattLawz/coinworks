a9f9c90ecc47404b482650e1a55412374d8c2a78
geenva <geenva@users.noreply.github.com>
2021-05-01 09:48:34 +0800

    refactor: move create to create/

---
+++ pages/create/create.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import { Heading, Stack, Center, Button, Input } from "@chakra-ui/react";
+import Container from "../../components/Container";
+
+export default function Create() {
+    return (
+        <>
+            <Container />
+            <Center>
+                <Stack spacing={8}>
+                    <Heading>Link Generator</Heading>
+                    <Input placeholder="Insert Bitcoin Address" id="insert" />
+                    <Input placeholder="Output" id="output" />
+                    <Button colorScheme="blue" variant="outline">
+                        Generate
+                    </Button>
+                </Stack>
+            </Center>
+        </>
+    );
+}
