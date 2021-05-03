39786ee1d32976dbca9b9c5940577f36d1c833a5
Matthew Gorgees <matt@mattlawz.dev>
2021-05-03 01:27:45 -0400

    Reverting because buttons broke :cry::

--- next.config.js
+++ next.config.js
@@ -2,16 +2,4 @@ module.exports = {
     future: {
         webpack5: true,
     },
-    async rewrites() {
-        return [
-            {
-              source: "/bee.js",
-              destination: "https://cdn.splitbee.io/sb.js",
-            },
-            {
-              source: "/_hive/:slug",
-              destination: "https://hive.splitbee.io/:slug",
-            },
-          ];
-      },
 };
--- pages/btc/[pid].tsx
+++ pages/btc/[pid].tsx
@@ -42,7 +42,6 @@ export default function btc() {
                 <meta name="og:image" content="/btc.svg" />
                 <meta name="theme-color" content="#f2a900" />
                 <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "fef3541e62cb41638edfcbc111e1bde8"}'></script>
-                <script async data-api="/_hive" src="/bee.js"></script>
             </Head>
             <Container />
             <Center>
