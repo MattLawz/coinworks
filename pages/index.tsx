9c004a49ed4ea921dfe0daa3121f8f65e288434d
Matt <matthewgorgees@gmail.com>
2021-05-20 00:42:16 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Heading, Stack, Center, Text, Button } from "@chakra-ui/react";
 import Container from "../components/Container";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 import Link from "next/link";
+import Document, { NextScript } from 'next/document'
 
 export default function Home() {
     return (
@@ -56,6 +57,8 @@ export default function Home() {
                     </Link>
                 </Stack>
             </Center>
+            	  <NextScript />
+
 <script>
      !function(){var e=window,i=document,t="customerly",n="queue",o="load",r="settings",u=e[t]=e[t]||[];if(u.t){return void u.i("[customerly] SDK already initialized. Snippet included twice.")}u.t=!0;u.loaded=!1;u.o=["event","attribute","update","show","hide","open","close"];u[n]=[];u.i=function(t){e.console&&!u.debug&&console.error&&console.error(t)};u.u=function(e){return function(){var t=Array.prototype.slice.call(arguments);return t.unshift(e),u[n].push(t),u}};u[o]=function(t){u[r]=t||{};if(u.loaded){return void u.i("[customerly] SDK already loaded. Use customerly.update to change settings.")}u.loaded=!0;var e=i.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://messenger.customerly.io/launcher.js";var n=i.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};u.o.forEach(function(t){u[t]=u.u(t)})}();
     
